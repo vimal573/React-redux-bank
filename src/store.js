ce3bfca4e0d7b83a32409f6f3353bbaf0f8503e8
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-24 12:13:01 +0530

    Creating a Redux Store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,6 @@
+import { type } from '@testing-library/user-event/dist/type';
+import { createStore } from 'redux';
+
 const intialState = {
   balance: 0,
   loan: 0,
@@ -10,17 +13,37 @@ function reducer(state = intialState, action) {
       return { ...state, balance: state.balance + action.payload };
     case 'account/withdrwan':
       return { ...state, balance: state.balance - action.payload };
-    case 'account/reaquestLoan':
+    case 'account/requestLoan':
       if (state.loan > 0) return;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.amount,
+      };
     case 'account/payLoan':
       return {
         ...state,
         loan: 0,
-        balance: state.balance - state.loan,
         loanPurpose: '',
+        balance: state.balance - state.loan,
       };
     default:
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: 'account/deposit', payload: 500 });
+store.dispatch({ type: 'account/withdrwan', payload: 200 });
+console.log(store.getState());
+
+store.dispatch({
+  type: 'account/requestLoan',
+  payload: { amount: 1300, loanPurpose: 'Buy a Car' },
+});
+console.log(store.getState());
+
+store.dispatch({ type: 'account/payLoan' });
+console.log(store.getState());
